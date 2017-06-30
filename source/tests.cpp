f811ae23c7b27851c03906aa1a1449b56e6ad22b
lissylasagne <lissy_lissy1@yahoo.de>
2017-06-30 16:01:23 +0200

    :heart:

--- source/tests.cpp
+++ source/tests.cpp
@@ -19,6 +19,7 @@
 // ----------------------------------
 
 //hallo
+//lissy ist cool
 TEST_CASE("getter shape","[shape]")
 {
 	Sphere s {"name", Material{"",Color{0.0f,0.0f,0.0f},Color{0.0f,0.0f,0.0f},Color{0.0f,0.0f,0.0f},0.0f},glm::vec3{0.0f},1.0f};
