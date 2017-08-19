db3bbe4c3cb04de588410a9a2c1c9d23272c8a56
KaroBe <karoline.engel.brehm@uni-weimar.de>
2017-08-19 17:38:14 +0200

    fixed typo, moved methods und c'tors to hit.cpp

--- framework/hit.hpp
+++ framework/hit.hpp
@@ -17,53 +17,19 @@ class Shape;
 struct Hit
 {
 	//default
-	Hit() :
-		m_hit{false},
-		m_distance{INFINITY},
-		m_intersection{glm::vec3{INFINITY}},
-		m_shape{nullptr}
-	{};
+	Hit();
 
 	//doesn't hit shape
-	Hit(std::shared_ptr<Shape> const& s) :
-		m_hit{false},
-		m_distance{INFINITY},
-		m_intersection{glm::vec3{INFINITY}},
-		m_shape{s}
-	{};
+	Hit(std::shared_ptr<Shape> const& s);
 
-	Hit(bool h, float d, glm::vec3 const& i, std::shared_ptr<Shape> const& s) :
-		m_hit{h},
-		m_distance{d},
-		m_intersection{i},
-		m_shape{s}
-	{};
+	Hit(bool h, float d, glm::vec3 const& i, std::shared_ptr<Shape> const& s);
 
 	// ggf mit swap / move dings ersetzen für mehr perfomanz ?
-	Hit& operator=(Hit const& temp_hit)
-	{
-		if(this != &temp_hit)
-			m_hit = temp_hit.m_hit;
-			m_distance = temp_hit.m_distance;
-			m_intersection = temp_hit.m_intersection;
-			m_shape = temp_hit.m_shape;
-		}
-		return *this;
-	}
+	Hit& operator=(Hit const& temp_hit);
 
-	bool operator==(Hit const& other)
-    {
-        if(m_hit==other.m_hit and 
-        	m_distance==other.m_distance and 
-        	m_intersection==other.m_intersection and 
-        	m_shape==other.m_shape)
-        {
-            return true;
-        }
-        else
-        	return false;
-    }
+	bool operator==(Hit const& other);
 
+	//Attribute
 	bool m_hit;
 	float m_distance;
 	glm::vec3 m_intersection;
