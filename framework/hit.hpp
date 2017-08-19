0283b0c6a3ca96bc6a8b12d312706cf90d0da989
KaroBe <karoline.engel.brehm@uni-weimar.de>
2017-08-19 17:48:21 +0200

    cpp erstellen ist schief gegangen, es funktioniert jetzt erstmal mit einer hpp alleine, compiliert und tests zu intersect laufen

--- framework/hit.hpp
+++ framework/hit.hpp
@@ -17,23 +17,54 @@ class Shape;
 struct Hit
 {
 	//default
-	Hit();
+	Hit() :
+		m_hit{false},
+		m_distance{INFINITY},
+		m_intersection{glm::vec3{INFINITY}},
+		m_shape{nullptr}
+	{};
 
 	//doesn't hit shape
-	Hit(std::shared_ptr<Shape> const& s);
+	Hit(std::shared_ptr<Shape> const& s) :
+		m_hit{false},
+		m_distance{INFINITY},
+		m_intersection{glm::vec3{INFINITY}},
+		m_shape{s}
+	{};
 
-	Hit(bool h, float d, glm::vec3 const& i, std::shared_ptr<Shape> const& s);
+	Hit(bool h, float d, glm::vec3 const& i, std::shared_ptr<Shape> const& s) :
+		m_hit{h},
+		m_distance{d},
+		m_intersection{i},
+		m_shape{s}
+	{};
 
 	// ggf mit swap / move dings ersetzen für mehr perfomanz ?
-	Hit& operator=(Hit const& temp_hit);
+	Hit& operator=(Hit const& temp_hit)
+	{
+		if(this != &temp_hit)
+		{
+			m_hit = temp_hit.m_hit;
+			m_distance = temp_hit.m_distance;
+			m_intersection = temp_hit.m_intersection;
+			m_shape = temp_hit.m_shape;
+		}
+		return *this;
+	}
 
-	bool operator==(Hit const& other);
+	bool operator==(Hit const& other)
+    {
+        if(m_hit==other.m_hit and 
+        	m_distance==other.m_distance and 
+        	m_intersection==other.m_intersection and 
+        	m_shape==other.m_shape)
+        {
+            return true;
+        }
+        else
+        	return false;
+    };
 
-	//Attribute
-	bool m_hit;
-	float m_distance;
-	glm::vec3 m_intersection;
-	std::shared_ptr<Shape> m_shape;
 
 	friend std::ostream& operator<<(std::ostream& os, Hit const& hit)
     {
@@ -42,7 +73,12 @@ struct Hit
             << "intersection: " << hit.m_intersection.x << " " << hit.m_intersection.y << " " << hit.m_intersection.z << "\n"
             << "shape: " << hit.m_shape -> get_name() << "\n";
         return os;
-    }
+	}
+
+	bool m_hit;
+	float m_distance;
+	glm::vec3 m_intersection;
+	std::shared_ptr<Shape> m_shape;
 };
 
 #endif //HIT_HPP
